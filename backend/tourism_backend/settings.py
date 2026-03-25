bf4b21894f33f317ecae96b338dd84dbeddeb8d1
rocksonowusu <orockson02@gmail.com>
2026-03-25 10:41:39 +0000

    added deployment link

--- backend/tourism_backend/settings.py
+++ backend/tourism_backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tourism-j3nz.onrender.com']
 
 
 # Application definition
